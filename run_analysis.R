f26a29833e3afdbb982dd4afb91a1b5fcbbd8f01
mixomix <mixomix@centrum.cz>
2017-01-08 20:46:53 +0100

    v1

---
+++ run_analysis.R
@@ -0,0 +1,9 @@
+# this script requires you to be in the working directory of the data 
+# - that is the one where README.txt of the dataset is
+
+name_list <- read.table("train/x_train.txt")
+
+train <- read.table("train/x_train.txt")
+
+
+names(train) <- 
